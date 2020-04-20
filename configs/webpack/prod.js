894ac28a6ce88da5ea070833e9bfb39e74160be3
venkat <venkatesa.r@solvezy.com>
2020-04-21 00:58:05 +0530

    code cc

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -8,7 +8,7 @@ module.exports = merge(commonConfig, {
   mode: 'production',
   entry: './index.tsx',
   output: {
-    filename: 'democart/js/bundle.[hash].min.js',
+    filename: 'js/bundle.[hash].min.js',
     path: resolve(__dirname, '../../dist'),
     publicPath: '/',
   },
