d9d48adcff096fb907d9a890eac78c9862f1358d
venkat <venkatesa.r@solvezy.com>
2020-04-21 11:25:59 +0530

    express

--- express.js
+++ express.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const portNumber = 3000;
+const portNumber = process.env.PORT || 3000;
 const sourceDir = 'dist';
 
 app.use(express.static(sourceDir));
 
+app.get("*", (req, res) => {
+    res.sendFile(`${__dirname}/dist/index.html`);
+  }
+});
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
   console.log(`Serving content from /${sourceDir}/`);
